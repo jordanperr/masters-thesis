f6254ad617b6c8e393e17d0fd015c05703375654
Jordan Perr-Sauer <jordan.perr-sauer@nrel.gov>
2022-10-15 18:26:43 -0600

    wrote collection script

--- distill_before_verify_experiment/4_collect_data.py
+++ distill_before_verify_experiment/4_collect_data.py
@@ -1,7 +1,45 @@
-import sys
 import pandas as pd
+import json
+import re
+import sys
 
-path=sys.argv[1]
 
-if __name__=="__main__":
-    experiments = pd.read_csv(path+"/index.csv").to_dict("records")
+name=sys.argv[1]
+
+with open(name+".json", "r") as fp:
+    config = json.load(fp)
+
+index = pd.read_csv(name+"/index.csv")
+
+results = []
+
+for row in index.itertuples():
+    result = pd.read_csv(name+"/"+row.uuid+"/student.stats.csv")
+    result["uuid"] = row.uuid
+
+    for property in config["properties"]:
+        with open(f"{name}/{row.uuid}/verify.{property}.result", "r") as fp:
+            verified_result, python_time = [a.strip() for a in fp.readlines()]
+            result[f"prop.{property}.result"] = verified_result
+            result[f"prop.{property}.python_time"] = float(python_time.split(":")[1])
+        
+        with open(f"{name}/{row.uuid}/verify.{property}.stdout", "r") as fp:
+            contents = fp.read()
+            if re.search("Proven safe before enumerate", contents) != None:
+                runtime_re = None
+                result_re = "Safe"
+            else:
+                runtime_re = re.search("Runtime: (\d+\.\d+)", contents).groups(0)[0]
+                result_re = re.search("Result: ([a-zA-Z\s]+)", contents).groups(0)[0]
+
+            result[f"prop.{property}.reported_runtime"] = runtime_re
+            result[f"prop.{property}.stdout_result"] = result_re
+
+    results.append(result)
+
+results_df = pd.concat(results)
+
+combined_results = pd.merge(index, results_df, on="uuid", how="left")
+
+combined_results.to_csv(f"{name}/results.csv", index=False)
+
