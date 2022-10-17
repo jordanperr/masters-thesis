c06ee4150e654df8cd2d35e7378e36ff4c77fb16
Jordan Perr-Sauer <jope8154@shas0115.rc.int.colorado.edu>
2022-10-17 15:03:47 -0600

    adding depth_val_alpine experiment

--- distill_before_verify_experiment/slurm_alpine_exp_depth_val_10_17_2022.sh
+++ distill_before_verify_experiment/slurm_alpine_exp_depth_val_10_17_2022.sh
@@ -8,5 +8,5 @@ conda activate ./distill-env
 
 export PYTHONPATH=/home/jope8154/projects/masters_thesis/src/nnenum/src:$PYTHONPATH
 
-bash 0_run_full_experiment.sh 
+bash 0_run_full_experiment.sh exp_alpine_depth_val_10_17_2022 
 
