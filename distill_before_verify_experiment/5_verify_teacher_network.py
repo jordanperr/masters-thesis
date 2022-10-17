a7c1cfd532f5236bc3cfde2e3af01ae12bb6964f
Jordan Perr-Sauer <jordan.perr-sauer@nrel.gov>
2022-10-17 12:45:01 -0600

    started playing with validation loss in experiment. Added visuals for miss rates

--- distill_before_verify_experiment/5_verify_teacher_network.py
+++ distill_before_verify_experiment/5_verify_teacher_network.py
@@ -28,19 +28,20 @@ def run_verify(params):
 
     pathlib.Path(name+f"/teacher/{params['uuid']}").mkdir(parents=True, exist_ok=True)
 
-    network_path = str(pathlib.Path("../data")/f"ACASXU_run2a_{tau}_{a_prev}_batch_2000.onnx")
+    network_path = str(pathlib.Path("../data")/f"acasxu/ACASXU_run2a_{params['tau']}_{params['a_prev']}_batch_2000.onnx")
 
     properties = ["1","2","3","4"]
 
     for property in properties:
-        result_path = name+f"/{config['uuid']}/verify.{property}.result"
-        stdout_path = name+f"/{config['uuid']}/verify.{property}.stdout"
+        result_path = name+f"/teacher/{params['uuid']}/verify.{property}.result"
+        stdout_path = name+f"/teacher/{params['uuid']}/verify.{property}.stdout"
 
         cmd = config["nnenum_cmd"].format(network_path=network_path,
             stdout_path=stdout_path,
             property=property,
             result_path=result_path)
         
+        print(cmd)
         start = time.perf_counter()
         subprocess.getoutput(cmd)
         verify_time = time.perf_counter() - start
@@ -52,7 +53,7 @@ def run_verify(params):
 
 if __name__=="__main__":
 
-    experiments = pd.read_csv(name+"/index.csv")["repetition","tau","a_prev"].drop_duplicates()
+    experiments = pd.read_csv(name+"/index.csv", usecols=["repetition","tau","a_prev"]).drop_duplicates()
     experiments["uuid"] = experiments.apply(lambda _: uuid.uuid4(), axis=1)
     experiments = experiments.to_dict("records")
 
