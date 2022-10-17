eda51219d3ca433028b32a26c1bd49fb706cc12d
Jordan Perr-Sauer <jope8154@login13.rc.colorado.edu>
2022-10-17 17:13:17 -0600

    add lscpu and conda  list to script

--- distill_before_verify_experiment/slurm_alpine_exp_depth_val_10_17_2022.sh
+++ distill_before_verify_experiment/slurm_alpine_exp_depth_val_10_17_2022.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=24
+#SBATCH --time=02:00:00
+#SBATCH --account=ucb-general
+#SBATCH --output=slurm_exp_alpine_depth_val_10_17_2022-%j.out
+#SBATCH --job-name distill-job
 
 module purge
 module load anaconda
@@ -12,3 +20,5 @@ export OMP_NUM_THREADS=1
 
 bash 0_run_full_experiment.sh exp_alpine_depth_val_10_17_2022 
 
+lscpu > exp_alpine_depth_val_10_17_2022/lscpu.out
+conda list --export > exp_alpine_depth_val_10_17_2022/requirements-freeze.txt
