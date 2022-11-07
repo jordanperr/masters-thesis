6479b8282be4fbbdb469f7f52e2cfb487c3bab44
Jordan Perr-Sauer <jordan.perr-sauer@nrel.gov>
2022-11-06 21:30:44 -0700

    updating slurm file

--- Readme.md
+++ Readme.md
@@ -1,2 +1,10 @@
 Thesis files
 Jordan Perr-Sauer, CU Boulder, 2022
+
+## Running on Summit
+
+1. Check out this repository
+2. Create distill-env anaconda environment.
+3. Modify slurm_summit_run_experiment and the json config files to reflect your environment.
+4. cd experiments/distill_before_verify_acasxu
+5. sbatch --export=DISTILL_JOB_NAME=exp_summit_test_11_06_2022 slurm_summit_run_experiment.sh
--- experiments/distill_before_verify_acasxu/slurm_summit_run_experiment.sh
+++ experiments/distill_before_verify_acasxu/slurm_summit_run_experiment.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sbatch <<EOT
-#!/bin/bash
 
 #SBATCH --account=ucb-general
 #SBATCH --partition=shas
@@ -8,24 +6,21 @@ sbatch <<EOT
 #SBATCH --nodes=1
 #SBATCH --ntasks=24
 #SBATCH --time=08:00:00
-#SBATCH -o="slurm_out."$1".txt"
-#SBATCH -e="slurm_err."$1".txt"
-#SBATCH --job-name distill-$1
+#SBATCH --job-name distill-job
 
 module purge
 module load anaconda
 conda activate /home/jope8154/projects/masters_thesis/distill-env
 
-lscpu > exp_alpine_depth_val_10_17_2022/lscpu.out
-conda list --export > exp_alpine_depth_val_10_17_2022/requirements-freeze.txt
-
 cd /home/jope8154/projects/masters_thesis/distill/experiments/distill_before_verify_experiment
+
+lscpu > slurm_lscpu.$DISTILL_JOB_NAME.txt
+conda list --export > slurm_requirements_freeze.$DISTILL_JOB_NAME.txt
 
 export PYTHONPATH=/home/jope8154/projects/masters_thesis/src/nnenum/src:$PYTHONPATH
 export OPENBLAS_NUM_THREADS=1
 export OMP_NUM_THREADS=1
 
-bash 0_run_full_experiment.sh $1
+bash 0_run_full_experiment.sh $DISTILL_JOB_NAME.json
 
 exit 0
-EOT
